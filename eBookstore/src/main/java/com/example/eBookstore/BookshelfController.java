b6ecf428b88b90f006d152a20b7aa7a3a8136632
Nikke Karaksela <nikke.karaksela@tuni.fi>
2022-12-09 13:42:24 +0200

    Update

--- eBookstore/src/main/java/com/example/eBookstore/BookshelfController.java
+++ eBookstore/src/main/java/com/example/eBookstore/BookshelfController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
 
@@ -19,5 +20,12 @@ public class BookshelfController {
 
         model.addAttribute("bookshelf", bookshelf);
         return "bookshelfPage";
+    }
+
+    @GetMapping("/bookshelf/{bookTitle}")
+    public String getBookInfo (@PathVariable String bookTitle, Model model) {
+        Book book = bookshelfService.findBookByTitle(bookTitle);
+        model.addAttribute("book", book);
+        return "bookPage";
     }
 }
--- eBookstore/src/main/java/com/example/eBookstore/BookshelfService.java
+++ eBookstore/src/main/java/com/example/eBookstore/BookshelfService.java
@@ -13,15 +13,15 @@ public class BookshelfService {
         this.bookshelf = new ArrayList<>();
 
         Book book1 = new Book("The Lord of the Rings", "J. R. R. Tolkien","1954-07-29","", "Fantasy", 20);
-        Book book2 = new Book("Pride and Prejudice", "Jane Austen","1813-1-28","", "Novel", 20);
-        Book book3 = new Book("Atomic Habits", "James Clear","2018-10-16","", "Self Help", 20);
-        Book book4 = new Book("Holy Bible", "Various Authors","","", "Religion", 20);
+        Book book2 = new Book("Pride and Prejudice", "Jane Austen","1813-1-28","", "Novel", 15);
+        Book book3 = new Book("Atomic Habits", "James Clear","2018-10-16","", "Self Help", 10);
+        Book book4 = new Book("Holy Bible", "Various Authors","","", "Religion", 0);
         Book book5 = new Book("The Handmaid’s Tale", "Margaret Atwood","1985","", "Dystopian Novel", 20);
         Book book6 = new Book("The Shining", "Stephen King","1977-1-28","", "Horror", 20);
         Book book7 = new Book("The Three Musketeers", "Alexandre Dumas","1844","", "Historical Fiction", 20);
         Book book8 = new Book("And Then There Were None", "Agatha Christie","1939-11-6","", "Mystery", 20);
-        Book book9 = new Book("Frankenstein", "Mary Shelley","1818","", "Sci Fi", 20);
-        Book book10 = new Book("A Christmas Carol", "Charles Dickens","","", "Christmas", 20);
+        Book book9 = new Book("Frankenstein", "Mary Shelley","1818","", "Sci Fi", 9.99);
+        Book book10 = new Book("A Christmas Carol", "Charles Dickens","","", "Christmas", 1.99);
 
         this.bookshelf.add(book1);
         this.bookshelf.add(book2);
@@ -37,5 +37,14 @@ public class BookshelfService {
 
     public ArrayList<Book> getBookshelf() {
         return this.bookshelf;
+    }
+
+    public Book findBookByTitle(String bookTitle) {
+        for (Book book : this.bookshelf) {
+            if (book.getTitle().equals(bookTitle)) {
+                return book;
+            }
+        }
+        return bookshelf.get(0);
     }
 }
